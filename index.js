7d94f7c577a9a0f9f74dbed78f9b52ecc6db4b58
SHOMANS <mohammedshoman9@gmail.com>
2022-12-07 10:31:52 +0200

    finish session 4

--- index.js
+++ index.js
@@ -1,196 +1,120 @@
-// const myObject = {
-//   name: 'Hadeel',
-//   age: 21,
-//   favColor: 'pink',
-// };
+// const myButton = document.querySelector('button');
 
-// console.log(myObject);
-// console.log(Object.keys(myObject)); // to get all keys of the object
-
-//-----------------------------------------------------
-
-// if (true) console.log('true');
-
-// if (true) {
-//   console.log('true');
-//   console.log('true2');
-// }
-
-// if (1 === 2) {
-//   console.log('true');
-//   console.log('true2');
-// } else {
-//   console.log('not true');
-// }
-
-// if (1 === 2) {
-//   console.log('true');
-//   console.log('true2');
-// } else if (1 === 3) {
-//   console.log('not true');
-// } else {
-//   console.log('not not true');
-// }
-
-//-----------------------------------------------------
-
-// true === 1 ? console.log(true) : console.log(false);
-// true === 1 || false === 0 ? console.log(true) : console.log(false);
-
-//-----------------------------------------------------
-
-// console.log(Math.round(Math.random() * 10)); 0 to 10
-// console.log(Math.floor(Math.random() * 11)); 0 to 10
-// console.log(Math.ceil(Math.random() * 9)); 1 to 10
-
-//-----------------------------------------------------
-
-// const color = 'green';
-
-// switch (color) {
-//   case 'red':
-//     console.log('its Red');
-//     break;
-//   case 'blue':
-//     console.log('its blue');
-//     break;
-//   default:
-//     console.log('its not one of the options');
-// }
-
-//-----------------------------------------------------
-
-// for (let i = 0; i < 10; i++) {
-//   console.log(i);
-// }
-
-// let i = 0;
-// while (i < 10) {
-//   console.log(i);
-//   i++;
-// }
-
-//-----------------------------------------------------
-
-// const arr = ['a', 'b', 'c', 'd'];
-
-// for (let i = 0; i < arr.length; i++) {
-//   console.log(i, arr[i]);
-// }
-
-// for (let item in arr) {
-//   console.log(item, arr[item]);
-// }
-
-// for (let item of arr) {
-//   console.log(item);
-// }
-
-//-----------------------------------------------------
-
-// const myObject = {
-//   name: 'Hadeel',
-//   age: 21,
-//   favColor: 'pink',
-// };
-
-// for (let item in myObject) {
-//   console.log(item);
-// }
-
-// for (let item of myObject) {
-//   console.log(item);
-// }
-
-//-----------------------------------------------------
-
-// const myStr = 5678;
-
-// const myArr = [...myStr.toString()];
-
-// console.log(myArr);
-
-//-----------------------------------------------------
-
-// const myFunc = (name = 'Name not found') => {
-//   console.log(name);
-// };
-
-// myFunc('Matar');
-// myFunc();
-// myFunc();
-
-//-----------------------------------------------------
-
-// function sum(name, ...hadeel) {
-//   let sum = 0;
-//   for (let arg of hadeel) sum += arg;
-//   console.log(name);
-//   return sum;
-// }
-
-// let x = sum('4', 9, 16);
-
-// console.log(x);
-
-//-----------------------------------------------------
-
-// const myDiv = document.querySelector('div');
-// const myDiv = document.getElementById('myDev');
-// const myInput = document.getElementById('myInput');
-// const myAnchor = document.querySelector('a');
-// console.log(myDiv);
-
-// myDiv.innerHTML = 'not Hello';
-
-// myAnchor.target = '_blank';
-// myAnchor.href = 'https://google.com';
-
-// myDiv.style.backgroundColor = 'red';
-
-// console.log(myDiv.style);
-
-// myDiv.addEventListener('mousemove', function () {
-//   myDiv.style.backgroundColor = 'blue';
-// });
-
-// myDiv.addEventListener('mouseleave', function () {
-//   myDiv.style.backgroundColor = 'red';
-// });
-
-// myAnchor.onclick = () => {
-//   myAnchor.style.color = 'pink';
-// };
-
-// myAnchor.onblur = () => {
-//   myAnchor.style.color = 'green';
-// };
-
-// myInput.addEventListener('keypress', (e) => {
-//   if (e.target.value.length > 50) {
-//     e.preventDefault();
+// window.onscroll = () => {
+//   if (window.scrollY > 200) {
+//     myButton.style.display = 'block';
+//   } else {
+//     myButton.style.display = 'none';
 //   }
-//   document.querySelector('span').innerHTML = 51 - e.target.value.length;
-//   console.log(50 - e.target.value.length);
+// };
+
+// myButton.addEventListener('click', () => {
+//   window.scrollTo(0, 0);
+//   // window.scrollBy(0, 100);
 // });
 
-//-----------------------------------------------------
+// ---------------------------------------------
 
-const myDiv = document.getElementById('myDiv');
-const myBtns = document.querySelectorAll('button');
+// const myButton = document.querySelector('button');
 
-const arrayData = ['text alsdjfhal', 'text not asdfkgjsd;fl', 'text is asdfkgjsd;fl', 'text are asdfkgjsd;fl'];
+// myButton.onclick = () => {
+//   window.location.assign('https://google.com');
+// };
 
-const dataObject = {
-  0: 'text alsdjfhal',
-  1: 'text not asdfkgjsd;fl',
-  2: 'text is asdfkgjsd;fl',
-  3: 'text are asdfkgjsd;fl',
-};
+// console.log(window.history);
 
-myBtns.forEach((button, index) => {
-  button.onclick = function () {
-    // myDiv.textContent = arrayData[index];
-    myDiv.textContent = dataObject[index];
+// ---------------------------------------------
+
+// window.console.log('asdfasd');
+// for (let i = 0; i < 2; i++) {
+//   let name = 'myName';
+//   console.log(window);
+// }
+
+// ---------------------------------------------
+
+// var x = 'global';
+// console.log(window);
+
+// const p = () => {
+//   var x = 'local';
+//   console.log(`local : ${x}   global : ${window.x}`);
+// };
+
+// p();
+
+// ---------------------------------------------
+
+// let x = 'global';
+// console.log(window);
+
+// const p = () => {
+//   let x = 'local';
+//   console.log(`local : ${x}   global : ${window.x}`);
+// };
+
+// p();
+
+// ---------------------------------------------
+
+// const myButton = document.querySelector('button');
+
+// let stopWatch = 0;
+
+// const count = setInterval(() => {
+//   stopWatch += 1;
+//   if (stopWatch === 3) {
+//     clearInterval(count);
+//   }
+//   console.log(stopWatch);
+// }, 1000);
+
+// myButton.onclick = () => {
+//   clearInterval(count);
+// };
+
+// ---------------------------------------------
+
+// const game = setTimeout(() => {
+//   console.log('game over');
+// }, 3000);
+
+// myButton.onclick = () => {
+//   clearTimeout(game);
+// };
+
+// ---------------------------------------------
+
+// const themeChange = (theme) => {
+//   console.log(theme);
+//   localStorage.setItem('theme', theme);
+//   document.body.style.backgroundColor = theme === 'dark' ? 'black' : 'white';
+// };
+
+// const darkButton = document.getElementById('dark');
+// const lightButton = document.getElementById('light');
+
+// darkButton.onclick = () => themeChange('dark');
+// lightButton.onclick = () => themeChange('light');
+
+// ---------------------------------------------
+
+const myButtons = document.querySelectorAll('button');
+
+// console.log(myButtons); // its a collection of buttons
+
+myButtons.forEach((button) => {
+  // we loop on the collection like the array and apply function on every button
+
+  // console.log(button);
+  button.onclick = () => {
+    // const theme = button.id;
+    const { id: theme } = button; // destructure the is from button parameter (object) then rename it to theme
+    console.log(theme);
+    localStorage.setItem('theme', theme);
+    document.body.style.backgroundColor = theme === 'dark' ? 'black' : 'white';
   };
 });
+
+const theme = localStorage.getItem('theme');
+document.body.style.backgroundColor = theme === 'dark' ? 'black' : 'white';
