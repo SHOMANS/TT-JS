54dffffb51d4f264e0487a792982b635c393dbed
SHOMANS <mohammedshoman9@gmail.com>
2022-12-11 13:24:50 +0200

    finish session 6 promises and async await

--- index.js
+++ index.js
@@ -1,126 +1,165 @@
-// const myButton = document.querySelector('button');
-// const myDiv = document.querySelector('div');
+// const callApi = (method, url) => {
+//   return new Promise((resolve, reject) => {
+//     const xhr = new XMLHttpRequest();
+//     xhr.onreadystatechange = () => {
+//       if (xhr.readyState === 4 && xhr.status === 200) {
+//         resolve(JSON.parse(xhr.response));
+//       } else if (xhr.readyState === 4 && xhr.status !== 200) {
+//         reject('sth went wrong');
+//       }
+//     };
 
-// const langs = {
-//   ar: {
-//     buttonText: 'en',
-//     divText: 'مرحبا أيها العالم',
-//   },
-//   en: {
-//     buttonText: 'العربية',
-//     divText: 'Hello World',
-//   },
-// };
-
-// let lang = localStorage.getItem('lang') || 'en';
-
-// const changeLangs = (lang) => {
-//   myButton.textContent = langs[lang].buttonText;
-//   myDiv.textContent = langs[lang].divText;
-//   // if (lang === 'en') {
-//   //   myButton.textContent = langs.en.buttonText;
-//   //   myDiv.textContent = langs.en.divText;
-//   // } else {
-//   //   myButton.textContent = langs.ar.buttonText;
-//   //   myDiv.textContent = langs.ar.divText;
-//   // }
-// };
-
-// const toggleLang = () => {
-//   lang = lang === 'en' ? 'ar' : 'en';
-//   window.localStorage.setItem('lang', lang);
-//   changeLangs(lang);
-// };
-
-// changeLangs(lang);
-
-// myButton.onclick = toggleLang;
-
-// --------------------------------------------
-
-// const xhr = new XMLHttpRequest();
-
-// xhr.onreadystatechange = () => {
-//   if (xhr.readyState === 4 && xhr.status === 200) {
-//     console.log(JSON.parse(xhr.response));
-//   } else if (xhr.readyState === 4 && xhr.status !== 200) {
-//     throw Error('sth went wrong');
-//   }
-// };
-
-// xhr.open('GET', 'https://jsonplaceholder.typicode.com/posts');
-// xhr.send();
-
-// const callApi = async (method, url) => {
-//   const xhr = new XMLHttpRequest();
-
-//   xhr.onreadystatechange = () => {
-//     if (xhr.readyState === 4 && xhr.status === 200) {
-//       return JSON.parse(xhr.response);
-//     } else if (xhr.readyState === 4 && xhr.status !== 200) {
-//       throw Error('sth went wrong');
-//     }
-//   };
-
-//   xhr.open(method, url, true);
-//   xhr.send();
+//     xhr.open(method, url);
+//     xhr.send();
+//   });
 // };
 
 // callApi('GET', 'https://jsonplaceholder.typicode.com/posts').then((res) => console.log(res));
 
 // --------------------------------------------
 
-// const myPost = {
-//   userId: 1,
-//   title4234: 'sunt aut facere repellat provident occaecati excepturi optio reprehenderit',
-//   body: 'quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto',
-// };
+// const myPromise = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     // resolve('foo');
+//     if (true) {
+//       resolve('done');
+//     } else {
+//       reject('failed');
+//     }
+//   }, 3000);
+// });
 
-// fetch('https://jsonplaceholder.typicode.com/posts', {
-//   method: 'POST',
-//   headers: {
-//     'Content-Type': 'application/json',
-//   },
-//   body: JSON.stringify(myPost),
-// })
-//   .then((res) => res.json())
-//   .then((data) => console.log(data))
+// myPromise
+//   .then((handleFulfilledA) => {
+//     console.log(handleFulfilledA);
+//   })
 //   .catch((error) => {
-//     console.log('error', error);
+//     console.log(error);
+//   })
+//   .finally(() => {
+//     console.log('finally');
 //   });
 
-// console.log(myData);
+// console.log('after');
 
 // --------------------------------------------
 
-fetch('https://jsonplaceholder.typicode.com/posts/1', {})
-  .then((res) => res.json())
-  .then((data) => console.log(data))
-  .catch((error) => {
-    console.log('error', error);
+// const myFirstFunction = () => {
+//   mySecondFunction();
+//   console.log('first');
+// };
+
+// const mySecondFunction = () => {
+//   myThirdFunction();
+//   console.log('second');
+// };
+// const myThirdFunction = () => {
+//   console.log('third');
+// };
+
+// myFirstFunction();
+
+// --------------------------------------------
+
+// setTimeout(() => {
+//   console.log('first');
+// }, 2);
+
+// setTimeout(() => {
+//   console.log('second');
+// }, 1);
+
+// setTimeout(() => {
+//   console.log('third');
+// }, 0);
+
+// console.log('out');
+
+// --------------------------------------------
+
+// const myPromise = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     // resolve('foo');
+//     if (true) {
+//       resolve('done1');
+//     } else {
+//       reject('failed');
+//     }
+//   }, 3000);
+// }).then((data) => {
+//   console.log(data);
+// });
+
+// const myPromise2 = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     // resolve('foo');
+//     if (true) {
+//       resolve('done2');
+//     } else {
+//       reject('failed');
+//     }
+//   }, 2);
+// }).then((data) => {
+//   console.log(data);
+// });
+
+// (async function myFunction() {
+//   await myPromise;
+//   console.log('async1');
+//   await myPromise2;
+//   console.log('async2');
+// })();
+
+// --------------------------------------------
+
+// fetch('https://jsonplaceholder.typicode.com/todos/1')
+//   .then((response) => response.json())
+//   .then((json) =>
+//     fetch('https://jsonplaceholder.typicode.com/todos/1', {
+//       method: 'put',
+//       body: {
+//         title: 'delectus aut autem',
+//       },
+//     })
+//       .then((response) => response.json())
+//       .then()
+//   )
+//   .catch((err) => console.log(err))
+// --------------------------------------------
+
+// async function newFunc2() {}
+
+(async () => {
+  // get todo which has id 1
+  const response = await fetch('https://jsonplaceholder.typicode.com/todos/1');
+  const data = await response.json();
+  console.log('data', data);
+
+  // update todo which has id 1 that comes from the response of get
+  const res = await fetch('https://jsonplaceholder.typicode.com/todos/' + data.id, {
+    method: 'put',
+    body: JSON.stringify({
+      title: 'new',
+    }),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
   });
+  const data2 = await res.json();
+  console.log('data2', data2);
 
-// --------------------------------------------
-
-const myPost = {
-  userId: 1,
-  title: 'new',
-  body: 'new',
-};
-
-fetch('https://jsonplaceholder.typicode.com/posts/1', {
-  method: 'delete',
-  headers: {
-    'Content-Type': 'application/json',
-  },
-  // body: JSON.stringify(myPost),
-})
-  .then((res) => res.json())
-  .then((data) => console.log(data))
-  .catch((error) => {
-    console.log('error', error);
+  // update todo which has id 1 that comes from the response of get again
+  const res2 = await fetch('https://jsonplaceholder.typicode.com/todos/' + data.id, {
+    method: 'put',
+    body: JSON.stringify({
+      title: 'new test',
+    }),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
   });
+  const data3 = await res2.json();
+  console.log('data3', data3);
 
-console.log(myData);
-
-// --------------------------------------------
+  console.log('done');
+})();
